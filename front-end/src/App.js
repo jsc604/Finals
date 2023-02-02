95f00a40314fe233a81fe23a988d14d4ffa5adb8
Jeffrey Cheung <jeffreycheung_@live.com>
2023-02-02 14:30:59 -0800

    setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [nftData, setNftData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=canto&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(response => {
+        setNftData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Loading...</p>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {error ? <p>{error.toString()}</p> : <p>{JSON.stringify(nftData[0].current_price)}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
