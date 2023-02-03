19c84f6c30cb1c5cba41c8c33000bab20d097981
Jeffrey Cheung <jeffreycheung_@live.com>
2023-02-02 19:05:18 -0800

    added sample data and added it to table

--- front-end/src/components/CurrencyItems.jsx
+++ front-end/src/components/CurrencyItems.jsx
@@ -4,13 +4,13 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 export default function CurrencyItems(props) {
   return (
     <tr>
-      <td>bitcoin</td>
-      <td>BTC</td>
-      <td>44,793,749,260</td>
-      <td>23561</td>
-      <td>-0.87448</td>
-      <td>24196</td>
-      <td>23520</td>
+      <td>{props.name}</td>
+      <td>{props.symbol}</td>
+      <td>{props.volume}</td>
+      <td>{props.price}</td>
+      <td>{props.change}</td>
+      <td>{props.high}</td>
+      <td>{props.low}</td>
       <td><FontAwesomeIcon icon={faCaretDown} /></td>
     </tr>
   );
--- front-end/src/components/CurrencyTable.jsx
+++ front-end/src/components/CurrencyTable.jsx
@@ -1,6 +1,20 @@
 import CurrencyItems from "./CurrencyItems";
+import coinData from "../samples/coinData";
 
 export default function CurrencyTable(props) {
+  let currencyItems = coinData.map((coin, i) => (
+    <CurrencyItems
+      key={i}
+      name={coin.name}
+      symbol={coin.symbol}
+      volume={coin.total_volume}
+      price={coin.current_price}
+      change={coin.price_change_percentage_24h}
+      high={coin.high_24h}
+      low={coin.low_24h}
+    />
+  ));
+
   return (
     <table class="table table-bordered table-dark table-hover">
       <thead>
@@ -16,7 +30,7 @@ export default function CurrencyTable(props) {
         </tr>
       </thead>
       <tbody>
-        <CurrencyItems />
+        {currencyItems}
       </tbody>
     </table>
   );
