9fc4a25e09e4a15c46095954f5f97478ebd98417
Jeffrey Cheung <jeffreycheung_@live.com>
2023-02-02 17:25:13 -0800

    set up table files

---
+++ front-end/src/components/CurrencyItems.jsx
@@ -0,0 +1,13 @@
+export default function CurrencyItems(props) {
+  return (
+    <tr>
+      <td>bitcoin</td>
+      <td>BTC</td>
+      <td>44,793,749,260</td>
+      <td>23561</td>
+      <td>-0.87448</td>
+      <td>24196</td>
+      <td>23520</td>
+    </tr>
+  );
+};
---
+++ front-end/src/components/CurrencyTable.jsx
@@ -0,0 +1,22 @@
+import CurrencyItems from "./CurrencyItems";
+
+export default function CurrencyTable(props) {
+  return (
+    <table class="table table-bordered table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Symbol</th>
+          <th scope="col">Volume(usd)</th>
+          <th scope="col">Price(usd)</th>
+          <th scope="col">24 hr change(%)</th>
+          <th scope="col">24 hr high(usd)</th>
+          <th scope="col">24 hr low(usd)</th>
+        </tr>
+      </thead>
+      <tbody>
+       {CurrencyItems}
+      </tbody>
+    </table>
+  );
+};
