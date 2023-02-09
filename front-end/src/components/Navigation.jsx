11e7fed969d3256ebd6929cec92719b101646f4c
Jeffrey Cheung <jeffreycheung_@live.com>
2023-02-08 17:55:24 -0800

    changed link to wrap buttons

--- front-end/src/components/Navigation.jsx
+++ front-end/src/components/Navigation.jsx
@@ -2,23 +2,31 @@ import { Link } from "react-router-dom";
 import classNames from "classnames";
 
 export default function Navigation(props) {
+  const { toggle } = props;
 
   const stockButton = classNames('btn btn-outline-warning', { 'btn btn-outline-warning active': props.tab === 'stocks' });
   const cryptoButton = classNames('btn btn-outline-warning', { 'btn btn-outline-warning active': props.tab === 'crypto' });
   const nftButton = classNames('btn btn-outline-warning', { 'btn btn-outline-warning active': props.tab === 'nft' });
+  
 
   return (
     <nav className="navbar navbar-light bg-dark">
       <div>
-        <button className={stockButton} type="button">
-          <Link to="/stocks/dashboard">Stocks</Link>
-        </button>
-        <button className={cryptoButton} type="button">
-          <Link to="/crypto/dashboard">Crypto</Link>
-        </button>
-        <button className={nftButton} type="button">
-          <Link to="/nft/dashboard">NFT's</Link>
-        </button>
+        <Link to="/stocks/dashboard">
+          <button className={stockButton} type="button">
+            Stocks
+          </button>
+        </Link>
+        <Link to="/crypto/dashboard">
+          <button className={cryptoButton} type="button">
+            Crypto
+          </button>
+        </Link>
+        <Link to="/nft/dashboard">
+          <button className={nftButton} type="button">
+            NFT's
+          </button>
+        </Link>
       </div>
 
       <div>
--- front-end/src/components/dashboards/CryptoDashboard.jsx
+++ front-end/src/components/dashboards/CryptoDashboard.jsx
@@ -1,14 +1,22 @@
 import CryptoTable from "../dataTables/CryptoTable";
 import useCryptoData from "../../hooks/useCryptoData";
 import Navigation from "../Navigation";
+import { useState } from "react";
 
 export default function CryptoDashboard(props) {
+  const [watchlist, setWatchlist] = useState(false);
+
   const { cryptoData } = useCryptoData('/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'); 
+
+  const watchlistToggle = () => {
+    setWatchlist(watchlist === false ? true : false);
+  };
+
 
   return (
     <main>
       <h1><strong>Top Crypto Currencies</strong></h1>
-      <Navigation tab={'crypto'} />
+      <Navigation tab={'crypto'} toggle={watchlistToggle}/>
       {cryptoData && <CryptoTable data={cryptoData} />}
     </main>
   );
